4dfaa39f5f4dbe2988146a06e5c4d01a7456cfb9
yangjaehyuk <gndksakxk10@naver.com>
2022-02-13 01:24:41 +0900

    add python file

--- Jae/스터디 4주차/1912.py
+++
@@ -1,8 +0,0 @@
-a=int(input())
-b=list(map(int,input().split(' ')))
-dp=[0]*a
-for i in range(a):
-    dp[i]=max(b[i],dp[i-1]+b[i])
-    """dp=10,6,9,10,15,21,-14,12,33,32"""
-    """즉, dp[i-1]+b[i]가 b[i]보다 작으면 dp[i]의 값은 b[i]의 값으로 초기화 되어야 함"""
-print(max(dp))
